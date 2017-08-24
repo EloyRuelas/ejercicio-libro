ab76a1e67fab62c90163ef2757b77f3ce4224081
Eloy Ruelas <eloy.rue@hotmail.com>
2017-08-24 10:33:07 -0500

    Se creo el constructor para darle valores inicales a tiulo y autor

---
+++ Libro.java
@@ -0,0 +1,14 @@
+public class Libro
+{
+    private String titulo;
+    private String autor;
+    
+    public Libro(String iniTitulo, String iniAutor)
+    {
+        titulo=iniTitulo;
+        autor=iniAutor;
+    }
+}
+    
+    
+    
